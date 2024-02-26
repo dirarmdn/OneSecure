fb98ea396c82872a00798048b12112ab5127e0f4
Raihan <marudever@gmail.com>
2024-02-26 08:57:13 +0700

    adding header file for aes

---
+++ aes.h
@@ -0,0 +1,16 @@
+#ifndef AES_H
+#define AES_H
+
+#include <stdint.h>
+#include <stdlib.h>
+
+// Data Type
+typedef uint8_t byte;
+typedef uint32_t word;
+
+// Constant
+#define AES_BLOCK_SIZE 16
+
+// 
+
+#endif
