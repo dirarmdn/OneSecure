2bc833f23477bea0fd5f78c42d7663b8ed500294
Raihan <marudever@gmail.com>
2024-02-26 09:52:04 +0700

    restructuring aesh.h

--- aes.h
+++ aes.h
@@ -1,60 +1,43 @@
 #ifndef AES_H
 #define AES_H
 
-#include <stdint.h>
-#include <stddef.h>
+// Define AES Key Sizes
+#define AES_128_KEY_SIZE 16
+#define AES_192_KEY_SIZE 24
+#define AES_256_KEY_SIZE 32
 
-// Data Type
-typedef uint8_t byte;
-typedef uint32_t word;
-
-// Constant
+// Define AES Block Size
 #define AES_BLOCK_SIZE 16
 
-// Size and Round Key
-#ifdef AES128
-#define AES_KEY_SIZE 16
-#define AES_ROUNDS 10
-#elif defined(AES192)
-#define AES_KEY_SIZE 24
-#define AES_ROUNDS 12
-#elif defined(AES256)
-#define AES_KEY_SIZE 32
-#define AES_ROUNDS 14
-#else
-#error "Ukuran key tidak valid!"
-#endif
-
-// Operation Mode
+// Define Operation Mode
 typedef enum {
-    AES_MODE_ECB,
-    AES_MODE_CBC,
-    AES_MODE_CTR,
-} aes_mode_t;
+    ECB,
+    CBC,
+    CTR,
+} AES_MODE;
 
-// Data Structure
+// Define AES Key Structures
 typedef struct {
-    byte bytes[AES_BLOCK_SIZE];
-} aes_state_t;
+    int size;               // size of the key in bytes
+    unsigned char *data;    // pointer to the key data
+} AESKey;
 
+// Define AES Context Structure
 typedef struct {
-    word words[AES_ROUNDS + 1];
-} aes_round_key_t;
+    AESKey key;                         // encrypt/decrypt key
+    unsigned char iv[AES_BLOCK_SIZE];   // initialization vector
+    AES_MODE mode;                      // operation mode
+} AESContext;
 
-// Function Declaration
-void aes_init_state(aes_state_t *state);
-void aes_init_round_key(aes_round_key_t *round_key, const byte *key);
-void aes_sub_bytes(aes_state_t *state);
-void aes_shift_rows(aes_state_t *state);
-void aes_mix_columns(aes_state_t *state);
-void aes_add_round_key(aes_state_t *state, const aes_round_key_t *round_key);
-void aes_encrypt_block(aes_state_t *state, const byte *input, byte *output);
-void aes_decrypt_block(aes_state_t *state, const byte *input, byte *output);
-void aes_encrypt(const byte *input, byte *output, size_t length, aes_mode_t mode, const byte *iv);
-void aes_decrypt(const byte *input, byte *output, size_t length, aes_mode_t mode, const byte *iv);
+// Function Prototypes
+void aes_init(AESContext *ctx, unsigned char *key, int key_size, unsigned char *iv, AES_MODE mode);
+void aes_encrypt(AESContext *ctx, unsigned char *input, unsigned char *output, int length);
+void aes_decrypt(AESContext *ctx, unsigned char *input, unsigned char *output, int length);
 
-// Macro
-#define AES_GET_BYTE(state, i) state->bytes[i]
-#define AES_SET_BYTE(state, i, value) state->bytes[i] = value
+// AES Core Functions
+void AddRoundKey(unsigned char state[AES_BLOCK_SIZE], unsigned char *roundKey);
+void SubBytes(unsigned char state[AES_BLOCK_SIZE]);
+void ShiftRows(unsigned char state[AES_BLOCK_SIZE]);
+void MixColumns(unsigned char state[AES_BLOCK_SIZE]);
 
 #endif
