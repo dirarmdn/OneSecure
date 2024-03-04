12d752dbce60af29306fe3205745e37dac72d9b4
Alya Gustiani Nur 'Afifah <alya.gustiani.tif23@polban.ac.id>
2024-03-04 08:06:50 +0700

    menambahkan header dan function untuk subbytes dan shiftrow

--- aes.c
+++ aes.c
@@ -3,6 +3,36 @@
 #include <string.h>
 #include "aes.h"
 
-void subBytes(){
-	
+
+
+void subBytes(unsigned char *state)
+{
+    int i;
+    /* substitute all the values from the state with the value in the SBox
+     * using the state value as index for the SBox
+     */
+    for (i = 0; i < 16; i++)
+        state[i] = getSBoxValue(state[i]);
+}
+
+void shiftRows(unsigned char *state)
+{
+    int i;
+    // iterate over the 4 rows and call shiftRow() with that row
+    for (i = 0; i < 4; i++)
+        shiftRow(state + i * 4, i);
+}
+
+void shiftRow(unsigned char *state, unsigned char nbr)
+{
+    int i, j;
+    unsigned char tmp;
+    // each iteration shifts the row to the left by 1
+    for (i = 0; i < nbr; i++)
+    {
+        tmp = state[0];
+        for (j = 0; j < 3; j++)
+            state[j] = state[j + 1];
+        state[3] = tmp;
+    }
 }
--- aes.h
+++ aes.h
@@ -8,6 +8,12 @@
 typedef uint8_t byte;
 typedef uint32_t word;
 
+// Implementation: subBytes
+void subBytes(unsigned char *state);
+// Implementation: shiftRows
+void shiftRows(unsigned char *state);
+void shiftRow(unsigned char *state, unsigned char nbr);
+
 // Constant
 #define AES_BLOCK_SIZE 16
 
