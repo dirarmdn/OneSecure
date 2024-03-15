43a6201f6ef315f19696b14d064e2676996b8cb6
D Ali RI <148074211+DAliRIJTK@users.noreply.github.com>
2024-03-15 22:06:26 +0700

    OneSecure.dev
    Menggabungkan pvd dengan aes

--- main.c
+++ main.c
@@ -10,6 +10,11 @@
 #include "dhira.h"
 #include "dwika.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
 int main(int argc, char *argv[])
 {
     // the expanded keySize
