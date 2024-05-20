f2a70ef8b4f111f2c11825ebcdb0df56fe4b9bbf
DAliRIJTK <148074211+DAliRIJTK@users.noreply.github.com>
2024-05-20 08:31:44 +0700

    update main_AES.c

--- Header_Body/main_AES.c
+++ Header_Body/main_AES.c
@@ -11,9 +11,10 @@
 #define MAX_TEXT_LENGTH 16
 
 int main() {
-
     int option = 1;
-    int i, choice, format;
+    int i, choice;
+    int rotationSteps = 3; // Misalnya jumlah langkah rotasi yang digunakan
+    int rotationDirection = 1; // 1 untuk rotasi ke kanan, 0 untuk rotasi ke kiri
     char inputText[MAX_TEXT_LENGTH];
     unsigned char plaintext[MAX_TEXT_LENGTH];
     unsigned char ciphertext[MAX_TEXT_LENGTH];
@@ -24,33 +25,10 @@ int main() {
     const char* secretMessage;
 	const char* coverImage;
     char menu;
-    int rotationSteps = 3; // Misalnya jumlah langkah rotasi yang digunakan
-    int rotationDirection = 1; // 1 untuk rotasi ke kanan, 0 untuk rotasi ke kiri
     Node* head = NULL; // Deklarasi head di sini untuk menghindari redefinisi
     
-<<<<<<< HEAD
     do {
         switch (option) {
-=======
-    
-    for (;;) {
-        // system("cls");
-        printf("||=================================================||\n");
-        printf("||                WELCOME TO OneSecure             ||\n");
-        printf("||=================================================||\n");
-    
-        printf("\nSelect an option:\n");
-        printf("1. Encrypt with AES\n");
-        printf("2. Decrypt with AES\n");
-        printf("3. Encrypt with PVD\n");
-        printf("4. Decrypt with PVD\n");
-        printf("0. Exit\n");
-        printf("Option (Only Real Number): ");
-        scanf("%d", &option);
-        
-        switch (option)
-        {
->>>>>>> 866b784af549a1610ec24a0cf1b0665a227ba451
             case 1:
                 system("cls");
                 printf("||=================================================||\n");
@@ -78,71 +56,64 @@ int main() {
                 option  = 1;
 
             case 2:
-            do {
-                system("cls");
-                printf("=========================     OneSecure Encrypt AES     =========================\n");
-                printf("\nInput your message (up to 16 characters): ");
-                scanf(" %[^\n]s", inputText);
-                memset(plaintext, 0, MAX_TEXT_LENGTH);
-                memcpy(plaintext, inputText, strlen(inputText));
+                do {
+                    system("cls");
+                    printf("=========================     OneSecure Encrypt AES     =========================\n");
+                    printf("\nInput your message (up to 16 characters): ");
+                    scanf(" %[^\n]s", inputText);
+                    memset(plaintext, 0, MAX_TEXT_LENGTH);
+                    memcpy(plaintext, inputText, strlen(inputText));
+                    
+                    if (strlen(inputText) > MAX_TEXT_LENGTH) {
+                        printf("\nError: Plaintext is too long. Please enter up to 16 characters.\n");
+                    }
+                } while (strlen(inputText) > MAX_TEXT_LENGTH);
                 
-                if (strlen(inputText) > MAX_TEXT_LENGTH) {
-                    printf("\nError: Plaintext is too long. Please enter up to 16 characters.\n");
-                }
-            } while (strlen(inputText) > MAX_TEXT_LENGTH);
-            
-            printf("Enter AES Key (16 characters): ");
-            scanf(" %[^\n]s", key);
+                printf("Enter AES Key (16 characters): ");
+                scanf(" %[^\n]s", key);
 
-            aes_encrypt(plaintext, ciphertext, key, size);
-            struct Node *encryptedLinkedList;
-            outputToLinkedList(plaintext, &encryptedLinkedList);
+                aes_encrypt(plaintext, ciphertext, key, size);
+                struct Node *encryptedLinkedList;
+                outputToLinkedList(plaintext, &encryptedLinkedList);
 
-            printf("Hiding Your Message\nProcessing ...");
-            sleep(2);
-            printf("\nYour Message Hide Successfully\n");
-            
-            printf("\n(VERY IMPORTANT!!!) keep it in your head\nCiphertext (HEXADECIMAL):\n");
-            printHex(ciphertext, MAX_TEXT_LENGTH);
-            system("pause");
-            option = 1;
+                printf("Hiding Your Message\nProcessing ...");
+                sleep(2);
+                printf("\nYour Message Hide Successfully\n");
+                
+                printf("\n(VERY IMPORTANT!!!) keep it in your head\nCiphertext (HEXADECIMAL):\n");
+                printHex(ciphertext, MAX_TEXT_LENGTH);
+                system("pause");
+                option = 1;
 
-        case 3:
-            do {
-            system("cls"); 
-	            printf("=========================     OneSecure Decrypt AES     =========================\n");
-	            printf("\nInput Your Ciphertext In HEXADECIMAL (separated by space): ");
-	            for (i = 0; i < MAX_TEXT_LENGTH; i++) {
-	                if (scanf("%2x", &ciphertext[i]) != 1) {
-	                    printf("\nError: Invalid input. Please enter a valid Ciphertext.\n");
-	                    while (getchar() != '\n'); // Membersihkan sisa input
-	                    break; // Keluar dari loop
-	                }
-	            }
-			} while (i < MAX_TEXT_LENGTH);
-            
-            aes_decrypt(ciphertext, decryptedtext, key, size);
-            struct Node *decryptedLinkedList;
-            outputToLinkedList(plaintext, &decryptedLinkedList);
+            case 3:
+                do {
+                system("cls"); 
+                    printf("=========================     OneSecure Decrypt AES     =========================\n");
+                    printf("\nInput Your Ciphertext In HEXADECIMAL (separated by space): ");
+                    for (i = 0; i < MAX_TEXT_LENGTH; i++) {
+                        if (scanf("%2x", &ciphertext[i]) != 1) {
+                            printf("\nError: Invalid input. Please enter a valid Ciphertext.\n");
+                            while (getchar() != '\n'); // Membersihkan sisa input
+                            break; // Keluar dari loop
+                        }
+                    }
+                } while (i < MAX_TEXT_LENGTH);
+                
+                aes_decrypt(ciphertext, decryptedtext, key, size);
+                struct Node *decryptedLinkedList;
+                outputToLinkedList(plaintext, &decryptedLinkedList);
 
-            printf("Decrypt your message\n Processing...\n");
-            sleep(2);
-            printf("\nDecrypted text (HEXADECIMAL):\n");
-            printHex(decryptedtext, MAX_TEXT_LENGTH);
-            
-            printf("\nDecrypted text (ASCII):\n");
-            printASCII(decryptedtext, MAX_TEXT_LENGTH);
-<<<<<<< HEAD
-            system("pause");
-            option = 1;
+                printf("Decrypt your message\n Processing...\n");
+                sleep(2);
+                printf("\nDecrypted text (HEXADECIMAL):\n");
+                printHex(decryptedtext, MAX_TEXT_LENGTH);
+                
+                printf("\nDecrypted text (ASCII):\n");
+                printASCII(decryptedtext, MAX_TEXT_LENGTH);
+                system("pause");
+                option = 1;
 
         case 4:
-=======
-            printf("\n\n\n");
-            break;
-            
-        case 3:
->>>>>>> 866b784af549a1610ec24a0cf1b0665a227ba451
             system("cls");
             printf("=========================     OneSecure Encrypt PVD     =========================\n");
             printf("1. Encrypt JPG file\n");
@@ -156,18 +127,23 @@ int main() {
             printf("Enter your output image name: ");
             scanf(" %s", stego_image);
             embed_process(cover_image, secret_message, stego_image, option);
+            system("pause");
+
         case 5:
             system("cls");
             printf("=========================     OneSecure Decrypt PVD     =========================\n");
             printf("Enter your image name: ");
             scanf(" %s", stego_image);
             extractMessage(stego_image);
+            system("pause");
             option = 1;
+
         case 0:
             printf("\nExiting...\n");
             sleep(2);
             exit(0);
             break;
+
         default:
             printf("\n invalid option. Please try again.\n");
             break;
