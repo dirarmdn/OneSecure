b0b719ae3953970a0f8dd0e8a94dcc5ca3af2709
dirarmdn <dhiraramadini@gmail.com>
2024-05-21 21:07:56 +0700

    fix: few changes

--- Header_Body/syahid.h
+++ Header_Body/syahid.h
@@ -1,17 +1,6 @@
 #ifndef SYAHID_H
 #define SYAHID_H
-
-#define data(P) (P)->data
-#define next(P) (P)->next
-#define prev(P) (P)->prev
-
-typedef char infotype;
-typedef struct TLinked *address;
-typedef struct TLinked {
-    infotype data;
-    address next;
-    address prev;
-} linkedList;
+#include "dhira.h"
 
 // Tujuan : Mengekstrak pesan rahasia yang sudah disisipkan dalam gambar stego
 void extractMessage(const char* stegoImage);
